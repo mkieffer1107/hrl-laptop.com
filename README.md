fc85d7f7db0f6a45c5db29c83ee76d4ddae82ecc
mkieffer1107 <mkieffer1107@gmail.com>
2024-09-13 04:26:48 -0400

    big

---
+++ README.md
@@ -0,0 +1,4 @@
+# Laptop Open on Hover CSS3 Animation
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/designersnest/pen/gOaXPbr](https://codepen.io/designersnest/pen/gOaXPbr).
+
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.html",  // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, "dist"),  // Output directory
+    filename: "bundle.js",  // Name of the bundled JavaScript file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+              outputPath: "assets/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+};
